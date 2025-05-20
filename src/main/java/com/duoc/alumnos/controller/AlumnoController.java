561fbd2c3a253e7fa57db4fde64bdfdc774e7fd5
PAAPCA03LC0710 <Duoc@PAAPCA03LC0710.duoc.acad>
2025-05-19 21:53:05 -0400

    Actualización 19/05

--- src/main/java/com/duoc/alumnos/controller/AlumnoController.java
+++ src/main/java/com/duoc/alumnos/controller/AlumnoController.java
@@ -1,12 +1,14 @@
 package com.duoc.alumnos.controller;
 
 
+import com.duoc.alumnos.model.Alumno;
 import com.duoc.alumnos.repository.AlumnoRepository;
 import com.duoc.alumnos.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AlumnoController {
@@ -22,6 +24,41 @@ public class AlumnoController {
 
     @GetMapping("/api/v1/alumnos")
     public ResponseEntity<?> getAlumnos() {
+
         return ResponseEntity.status(200).body(alumnoService.findAll());
-    }
+
 }
+
+    @GetMapping("/api/v1/alumno/{id}")
+    public ResponseEntity<?> getAlumno(@PathVariable Integer id) {
+        Alumno alumno = alumnoService.findById(id);
+
+        if (alumno == null) {
+            return ResponseEntity.status(404).body("Alumno no encontrado");
+        }
+        return ResponseEntity.status(200).body(alumno);
+    }
+
+    @DeleteMapping("/api/v1/alumno/{id}")
+    public ResponseEntity<?> deleteAlumno(@PathVariable Integer id) {
+        Alumno alumno = alumnoService.findById(id);
+        if (alumno == null) {
+            return ResponseEntity.status(404).body("Alumno no encontrado");
+        }
+
+        alumnoService.delete(id);
+        return ResponseEntity.status(200).body("Alumno eliminado");
+        }
+
+        @GetMapping("/api/v1/alumno/buscar")
+        public ResponseEntity<?> buscarAlumno(@RequestParam String correo) {
+            Alumno alumno = alumnoService.findByCorreo(correo);
+            return ResponseEntity.status(200).body(alumno);
+        }
+
+        @GetMapping("/api/v1/alumnos/{nombre}")
+        public ResponseEntity<?> buscarAlumnoPorNombre(@PathVariable String nombres) {
+            List<Alumno> alumnos = alumnoService.findByNombres(nombres);
+            return ResponseEntity.status(200).body(alumnos);
+        }
+    }
--- src/main/java/com/duoc/alumnos/service/AlumnoService.java
+++ src/main/java/com/duoc/alumnos/service/AlumnoService.java
@@ -15,7 +15,24 @@ public class AlumnoService {
     @Autowired
     public AlumnoRepository alumnoRepository;
 
+    //Traer a todos los alumnos registrados
     public List<Alumno> findAll() {
+
         return alumnoRepository.findAll();
+    }
+
+    //Traer un alumno consultado
+    public Alumno findById(Integer id) {
+        return alumnoRepository.findById(id).orElse(null);
+    }
+    public void delete(Integer id) {
+        alumnoRepository.deleteById(id);
+    }
+    //Metodo para buscar por correo
+    public Alumno findByCorreo(String correo) {
+        return alumnoRepository.findByCorreo(correo);
+    }
+    public List<Alumno> findByNombres(String nombres) {
+        return alumnoRepository.findByNombres(nombres);
     }
 }
